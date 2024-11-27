c85d63119b86c333cf70b84ac57508f5f35c795b
Alexander <sany199574@gmail.com>
2024-11-28 00:02:51 +0300

    Добавлена базовая структура папок

---
+++ src/TaskPlanner/PS.TaskPlanner.Web/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace PS.TaskPlanner.Web
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            return services;
+        }
+    }
+}
--- src/TaskPlanner/PS.TaskPlanner.Web/Program.cs
+++ src/TaskPlanner/PS.TaskPlanner.Web/Program.cs
@@ -1,6 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using PS.TaskPlanner.Application;
+using PS.TaskPlanner.Infrastructure;
+using PS.TaskPlanner.Web;
 
-builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services
+        .AddPresentation()
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
+}
 
 var app = builder.Build();
 
