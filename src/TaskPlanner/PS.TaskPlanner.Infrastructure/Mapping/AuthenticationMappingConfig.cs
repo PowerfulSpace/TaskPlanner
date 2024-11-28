69dbbf3f8c705cb1c0a30a9e92f58434bc8bb3a0
Alexander <sany199574@gmail.com>
2024-11-29 01:36:19 +0300

    Исправлена ошибка мапинга

--- src/TaskPlanner/PS.TaskPlanner.Infrastructure/Mapping/AuthenticationMappingConfig.cs
+++ src/TaskPlanner/PS.TaskPlanner.Infrastructure/Mapping/AuthenticationMappingConfig.cs
@@ -9,12 +9,12 @@ namespace PS.TaskPlanner.Infrastructure.Mapping
         public static void Configure()
         {
             TypeAdapterConfig<AuthUser, AppUser>.NewConfig()
-                .Map(dest => dest.Id, src => Guid.Parse(src.Id))
+                .Map(dest => dest.Id, src => src.Id)
                 .Map(dest => dest.UserName, src => src.UserName)
                 .Map(dest => dest.Email, src => src.Email);
 
             TypeAdapterConfig<AppUser, AuthUser>.NewConfig()
-                .Map(dest => dest.Id, src => src.Id.ToString())
+                .Map(dest => dest.Id, src => src.Id)
                 .Map(dest => dest.UserName, src => src.UserName)
                 .Map(dest => dest.Email, src => src.Email);
         }
