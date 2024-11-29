3b9fddfc46ca566b4e35488c83bd0cd3a1c56330
Alexander <sany199574@gmail.com>
2024-11-29 03:31:47 +0300

    реализован репоиторий AppUserRepository

--- src/TaskPlanner/PS.TaskPlanner.Infrastructure/Persistence/Repositories/AppUserRepository.cs
+++ src/TaskPlanner/PS.TaskPlanner.Infrastructure/Persistence/Repositories/AppUserRepository.cs
@@ -1,6 +1,53 @@
-﻿namespace PS.TaskPlanner.Infrastructure.Persistence.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using PS.TaskPlanner.Application.Common.Interfaces.Persistence;
+using PS.TaskPlanner.Domain.Entities;
+
+namespace PS.TaskPlanner.Infrastructure.Persistence.Repositories
 {
-    public class AppUserRepository
+    public class AppUserRepository : IAppUserRepository
     {
+        private readonly TaskPlannerDbContext _dbContext;
+
+        public AppUserRepository(TaskPlannerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<AppUser?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.AppUsers
+                .FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<AppUser?> GetByEmailAsync(string email)
+        {
+            return await _dbContext.AppUsers
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetAllAsync()
+        {
+            return await _dbContext.AppUsers.ToListAsync();
+        }
+
+        public async Task AddAsync(AppUser entity)
+        {
+            await _dbContext.AppUsers.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(AppUser entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var user = new AppUser { Id = id };
+            _dbContext.AppUsers.Attach(user);
+            _dbContext.Entry(user).State = EntityState.Deleted;
+            await _dbContext.SaveChangesAsync();
+        }
     }
 }
