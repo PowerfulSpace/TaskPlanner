d0630f663bc7b0e9da145bf5dd1bdef9a394348d
Alexander <sany199574@gmail.com>
2024-12-02 09:27:40 +0300

    Добавлен ValidationBehavior
    приведён в порядок DependencyInjection для слоя Application

--- src/TaskPlanner/PS.TaskPlanner.Application/DependencyInjection.cs
+++ src/TaskPlanner/PS.TaskPlanner.Application/DependencyInjection.cs
@@ -1,6 +1,10 @@
-﻿using Mapster;
+﻿using FluentValidation;
+using Mapster;
 using MapsterMapper;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using PS.TaskPlanner.Application.Behaviors;
+using System.Reflection;
 
 namespace PS.TaskPlanner.Application
 {
@@ -8,9 +12,10 @@ namespace PS.TaskPlanner.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-
             services
-                .AddMapsterConfiguration();
+                .AddMapsterConfiguration()
+                .AddMediatRConfiguration()
+                .AddValidationConfiguration();
 
             return services;
         }
@@ -22,6 +27,19 @@ namespace PS.TaskPlanner.Application
             services.AddSingleton(config);
             services.AddScoped<IMapper, ServiceMapper>();
 
+            return services;
+        }
+
+        private static IServiceCollection AddMediatRConfiguration(this IServiceCollection services)
+        {
+            services.AddMediatR(x => x.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+            return services;
+        }
+
+        private static IServiceCollection AddValidationConfiguration(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             return services;
         }
     }
