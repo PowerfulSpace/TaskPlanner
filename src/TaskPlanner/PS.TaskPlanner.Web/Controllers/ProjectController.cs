f87a5f96325708bc5b4ec06183a206bf8fa3a761
Alexander <sany199574@gmail.com>
2024-12-03 06:46:58 +0300

    Переписал контроллер ProjectController

--- src/TaskPlanner/PS.TaskPlanner.Web/Controllers/ProjectController.cs
+++ src/TaskPlanner/PS.TaskPlanner.Web/Controllers/ProjectController.cs
@@ -24,7 +24,9 @@ namespace PS.TaskPlanner.Web.Controllers
         // 1. Просмотр всех проектов
         public async Task<IActionResult> Index()
         {
-            var projects = await _mediator.Send(new GetAllProjectsQuery());
+            var query = new GetAllProjectsQuery();
+
+            var projects = await _mediator.Send(query);
             var viewModel = _mapper.Map<List<ProjectViewModel>>(projects);
 
             return View(viewModel);
@@ -33,6 +35,8 @@ namespace PS.TaskPlanner.Web.Controllers
         // 2. Просмотр проекта по ID
         public async Task<IActionResult> Details(Guid id)
         {
+            var query = new GetProjectByIdQuery { Id = id };
+
             var project = await _mediator.Send(new GetProjectByIdQuery { Id = id });
             if (project == null) return NotFound();
 
@@ -63,7 +67,9 @@ namespace PS.TaskPlanner.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> Edit(Guid id)
         {
-            var project = await _mediator.Send(new GetProjectByIdQuery { Id = id });
+            var query = new GetProjectByIdQuery { Id = id };
+
+            var project = await _mediator.Send(query);
             if (project == null) return NotFound();
 
             var viewModel = _mapper.Map<ProjectViewModel>(project);
@@ -73,9 +79,10 @@ namespace PS.TaskPlanner.Web.Controllers
         // 6. Редактирование проекта (POST)
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, ProjectViewModel model)
+        public async Task<IActionResult> Edit(ProjectViewModel model)
         {
-            if (id != model.Id) return BadRequest();
+            // Проверяем, что Id в модели совпадает с переданным Id
+            if (model.Id == Guid.Empty) return BadRequest("Invalid project ID");
 
             if (!ModelState.IsValid) return View(model);
 
@@ -88,7 +95,9 @@ namespace PS.TaskPlanner.Web.Controllers
         [HttpGet]
         public async Task<IActionResult> Delete(Guid id)
         {
-            var project = await _mediator.Send(new GetProjectByIdQuery { Id = id });
+            var query = new GetProjectByIdQuery { Id = id };
+
+            var project = await _mediator.Send(query);
             if (project == null) return NotFound();
 
             var viewModel = _mapper.Map<ProjectViewModel>(project);
@@ -98,9 +107,14 @@ namespace PS.TaskPlanner.Web.Controllers
         // 8. Удаление проекта (POST)
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        public async Task<IActionResult> DeleteConfirmed(ProjectViewModel model)
         {
-            await _mediator.Send(new DeleteProjectCommand { Id = id });
+            // Проверка на пустой ID
+            if (model.Id == Guid.Empty) return BadRequest("Invalid project ID");
+
+            var command = new DeleteProjectCommand { Id = model.Id };
+
+            await _mediator.Send(command);
             return RedirectToAction(nameof(Index));
         }
     }
